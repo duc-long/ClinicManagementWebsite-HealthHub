87504dc7f1e5ba09f0a4f6a2686ce8b72ae663a2
NgKiet <kietnq.ce191198@gmail.com>
2025-10-10 17:34:40 +0700

    feat: guest-view-doctor-list, ver0.2

--- src/main/java/com/group4/clinicmanagement/controller/HomeController.java
+++ src/main/java/com/group4/clinicmanagement/controller/HomeController.java
@@ -1,36 +1,43 @@
 package com.group4.clinicmanagement.controller;
 
 import com.group4.clinicmanagement.entity.Doctor;
-import com.group4.clinicmanagement.repository.DoctorRepositories;
+
 import com.group4.clinicmanagement.service.DoctorService;
-import org.springframework.data.domain.Page;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/guest")
-public class GuestController {
+@RequestMapping(value = "/healthhub")
+public class HomeController {
 
     private final DoctorService doctorService;
 
-    public GuestController(DoctorService doctorService) {
+    public HomeController(DoctorService doctorService) {
         this.doctorService = doctorService;
     }
 
-    @GetMapping
+    @GetMapping(value = "/home")
     public String guestHome(Model model) {
-        return "guest/HomeGuest";
+        return "home/HomeGuest";
     }
 
     @GetMapping(value = "/list-doctor")
     public String listDoctor(Model model) {
         List<Doctor> doctors = doctorService.findAllDoctors();
         model.addAttribute("doctors", doctors);
-        return "guest/doctor-list";
+        return "home/doctor-list";
+    }
+
+    @GetMapping(value = "/search-doctor")
+    public String searchDoctor(Model model) {
+        List<Doctor> doctors = doctorService.findAllDoctors();
+        model.addAttribute("doctors", doctors);
+        return "home/doctor-search";
     }
 }
