4bc65a386972b21f186b3b922214afb4cd3bfdd6
duc-long <longnvd712@gmail.com>
2025-10-12 02:56:53 +0700

    change class name to CashierRepository

--- src/main/java/com/group4/clinicmanagement/repository/CashierRepository.java
+++ src/main/java/com/group4/clinicmanagement/repository/CashierRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CashierRepositories extends JpaRepository<User,Integer> {
+public interface CashierRepository extends JpaRepository<User,Integer> {
 }
--- src/main/java/com/group4/clinicmanagement/service/CashierService.java
+++ src/main/java/com/group4/clinicmanagement/service/CashierService.java
@@ -1,26 +1,26 @@
 package com.group4.clinicmanagement.service;
 
 import com.group4.clinicmanagement.entity.User;
-import com.group4.clinicmanagement.repository.CashierRepositories;
+import com.group4.clinicmanagement.repository.CashierRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CashierService {
-    CashierRepositories cashierRepositories;
+    CashierRepository cashierRepository;
 
-    public CashierService(CashierRepositories cashierRepositories) {
-        this.cashierRepositories = cashierRepositories;
+    public CashierService(CashierRepository cashierRepository) {
+        this.cashierRepository = cashierRepository;
     }
 
     @Transactional
     public User findUserById(int id) {
-        return cashierRepositories.getReferenceById(id);
+        return cashierRepository.getReferenceById(id);
     }
 
     @Transactional
     public void save(User user) {
-        cashierRepositories.save(user);
+        cashierRepository.save(user);
     }
 
 }
