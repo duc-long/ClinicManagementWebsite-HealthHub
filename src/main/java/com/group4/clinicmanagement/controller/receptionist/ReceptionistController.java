3a3cba30219b632d026d9d136b5ff80878618e5f
duc-long <longnvd712@gmail.com>
2025-10-25 03:39:12 +0700

    update ui

--- src/main/java/com/group4/clinicmanagement/controller/receptionist/ReceptionistController.java
+++ src/main/java/com/group4/clinicmanagement/controller/receptionist/ReceptionistController.java
@@ -2,12 +2,15 @@ package com.group4.clinicmanagement.controller.receptionist;
 
 import com.group4.clinicmanagement.dto.ReceptionistUserDTO;
 import com.group4.clinicmanagement.dto.UserDTO;
+import com.group4.clinicmanagement.entity.Appointment;
 import com.group4.clinicmanagement.service.ReceptionistService;
 import com.group4.clinicmanagement.service.AppointmentService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/receptionist")
@@ -42,7 +45,22 @@ public class ReceptionistController {
     public String editProfile(@ModelAttribute("receptionist") ReceptionistUserDTO dto, Authentication authentication) {
         String receptionistName = authentication.getName();
 
-        receptionistService.updateReceptionistProfile(receptionistName,dto);
+        receptionistService.updateReceptionistProfile(receptionistName, dto);
         return "redirect:/receptionist/profile";
     }
+
+    @GetMapping("/appointment-list")
+    public String listAppointments(Model model) {
+        List<Appointment> appointments = appointmentService.getAllAppointments();
+        model.addAttribute("appointments", appointments);
+        return "receptionist/appointment-list";
+    }
+
+    @GetMapping("/appointment/{id}")
+    public String viewAppointmentDetails(@PathVariable("id") Integer id, Model model) {
+        Appointment appointment = appointmentService.getById(id);
+        model.addAttribute("appointment", appointment);
+        return "receptionist/appointment-details";
+    }
+
 }
